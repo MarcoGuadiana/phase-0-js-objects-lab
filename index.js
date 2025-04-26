6912ab650079ebbf2e4ea470892e15a7b984c2d4
Marco Guadiana <marcoguadiana01@gmail.com>
2025-04-26 18:50:44 -0500

    6:50PM

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 //Write your code here
 
+const attendee =  {attendeID: "T001" , name:"Alice Smith ", event: "JavaScript Conference" , ticketType: "VIP", ticketPrice: 150.00}
+  
+
+ attendee("T001", "Alice Smith", "JavaScript Conference", "VIP", 150.00);
+
+ function logAttendeeName(attendee) {
+  console.log(attendee.name); 
+ }
+
+function logTicketPrice (attendee) {
+  console.log(attendee.ticketPrice);
+
+}
+
+function updateTicketType (attendee) {
+  attendee.ticketType = newTicketType;
+  
+}
+
+function updateTicketPrice (attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
+
 
 
 //Needed for the tests to work. Don't modify
